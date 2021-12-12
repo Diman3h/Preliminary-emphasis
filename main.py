bd5b445247a55be9eda53b31588bebf34d963dab
Diman3h <79001845+Diman3h@users.noreply.github.com>
2021-12-12 19:41:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def emphasis(signal_batch, emph_coeff=0.95, pre=True):
-    """
-    """
-    result = np.zeros(signal_batch.shape)    # Форма голосового сигнала batch_sized заполнена 0
-    for sample_idx, sample in enumerate(signal_batch): Функция #enumerate (), которая отображает данные и индексы данных одновременно
-        for ch, channel_data in enumerate(sample):# Формула предыскажения y (n) = x (n) -ax (n-1)
-            if pre:
-                result[sample_idx][ch] = np.append(channel_data[0], channel_data[1:] - emph_coeff * channel_data[:-1])  
-            else:
-                result[sample_idx][ch] = np.append(channel_data[0], channel_data[1:] + emph_coeff * channel_data[:-1])
-    return result
+import numpy as np
+import wave
+import matplotlib.pyplot as plt
+f = wave.open(r"adx.wav", "rb")
+params = f.getparams()
+nchannels, sampwidth, framerate, nframes = params[:4]
+str_data = f.readframes(nframes)
+wave_data = np.fromstring(str_data, dtype=np.short)
+wave_data = wave_data*1.0/(max(abs(wave_data)))
+plt.specgram(wave_data,Fs = framerate, scale_by_freq = True, sides = 'default')
+plt.show()
